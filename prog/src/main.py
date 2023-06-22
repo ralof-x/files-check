fc81664363efade9ce04cfc7d213e011c124f9b8
ralof-x <ralof@gmx.net>
2023-06-22 17:16:44 +0200

    Build dictionary
    This function builds a dictionary that contains information about the
    file at the current iteration point of the program. This is then added
    to a larger dictionary that will resemble the structure of the directory
    it is currently operating in.

--- prog/src/main.py
+++ prog/src/main.py
@@ -1,3 +1,4 @@
+import datetime
 import pathlib
 from termcolor import colored
 import sys
@@ -17,6 +18,20 @@ def print_files(curr_dir, base_dir, level_counter):
     files = [item.name for item in sorted(curr_dir.glob('*')) if item.is_file()]
     for item in files:
         print(level_counter * '|-', item)
+
+
+def build_dict(file_object: pathlib.Path, base_dir):
+    file_dict = {}
+    if file_object.is_file():
+        file_dict["type"] = "file"
+        timestamp = file_object.stat().st_mtime
+        time = datetime.datetime.fromtimestamp(file_object.stat().st_mtime).strftime('%Y-%m-%d %H:%M:%S')
+        file_dict["last-modified"] = time
+        file_dict["size"] = round((file_object.stat().st_size / 1000), 1)
+        file_dict["name"] = file_object.name
+        path = str(file_object.resolve()).split(base_dir)
+        file_dict["location"] = path[-1]
+    return file_dict
 
 
 def main():
