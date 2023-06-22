34812f41bbf3fdfe3f8a3710ae709892a8ceee6d
ralof-x <ralof@gmx.net>
2023-06-22 17:12:06 +0200

    Update to keep track of start point
    I figured, that if I want to compare different directories,
    each function needs to know, from where it started

--- prog/src/main.py
+++ prog/src/main.py
@@ -3,24 +3,24 @@ from termcolor import colored
 import sys
 
 
-def walk_tree(directory, level_counter=1):
+def walk_tree(directory, base_dir, level_counter=1):
     path = pathlib.Path(directory)
-    print_files(path, level_counter)
+    print_files(path, base_dir, level_counter)
     dirs = [dir for dir in sorted(path.glob('*')) if dir.is_dir()]
     level_counter += 1
     for fold in dirs:
         print((level_counter - 1) * '|-', colored(fold.name, 'cyan', attrs=['bold']))
-        walk_tree(fold, level_counter)
+        walk_tree(fold, base_dir, level_counter)
 
 
-def print_files(dir, level_counter):
-    files = [item.name for item in sorted(dir.glob('*')) if item.is_file()]
+def print_files(curr_dir, base_dir, level_counter):
+    files = [item.name for item in sorted(curr_dir.glob('*')) if item.is_file()]
     for item in files:
         print(level_counter * '|-', item)
 
 
 def main():
-    walk_tree(sys.argv[1])
+    walk_tree(sys.argv[1], sys.argv[1])
 
 
 if __name__ == "__main__":
