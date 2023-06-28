a64e14b95e3ff9cc0ba2cd7e8d85845de29316d7
ralof-x <ralof@gmx.net>
2023-06-28 17:37:10 +0200

    Update tests
    make tests work by calling pytest from the main project directory

--- prog/test/test_dict_builder.py
+++ prog/test/test_dict_builder.py
@@ -1,17 +1,18 @@
 import pytest
 import pathlib
-from prog.src import main
-# from src import main
+import sys
+BASE_DIR = pathlib.Path.joinpath(pathlib.Path.cwd(), 'prog')
+print(BASE_DIR)
+sys.path.append(str(BASE_DIR))
+from src import main
 
 
 @pytest.fixture
 def get_dict_fixture():
-    file = "../../test_dir/dir1/neof"
-    # file_object = pathlib.Path(file)
-    # file_object = pathlib.Path.cwd().joinpath('..', '..', 'test_dir', 'dir1', 'neof')
-    file_object = pathlib.Path.cwd().parent.parent.joinpath('test_dir', 'dir1', 'neof')
-    # file_object = pathlib.Path.joinpath('..', '..', 'test_dir', 'dir1', 'neof')
-    return main.build_dict(file_object, file)
+    path = pathlib.Path.joinpath(pathlib.Path.cwd(), 'test_dir', 'dir1', 'neof')
+    file_object = path
+    base_dir = str(pathlib.Path.joinpath(BASE_DIR.parent, 'test_dir'))
+    return main.build_dict(file_object, base_dir)
 
 
 def test_test_working():
@@ -35,5 +36,4 @@ def test_dict_name(get_dict_fixture: dict):
 
 
 def test_dict_location(get_dict_fixture: dict):
-    assert get_dict_fixture["location"] == '/home/ralof/Documents/spare_time/Python/projects/chk_files/test_dir/dir1' \
-                                           '/neof'
+    assert get_dict_fixture["location"] == '/dir1/neof'
